bc79c1071aaad48c6b8719e0ec06e4aa7dad98a9
Alexandre 'Sunny' Kovalenko <techxorcist@outlook.com>
2025-04-09 11:27:58 -0400

    First working version of the duplicate file finder

--- BuildTargets/FindDuplicateFiles/FindDuplicateFiles.go
+++ BuildTargets/FindDuplicateFiles/FindDuplicateFiles.go
@@ -18,9 +18,34 @@ func main() {
 		log.Printf("Failed to enumerate %s (%v)\n", rootDirectory, err)
 		os.Exit(-1)
 	}
+	csMap := make(map[string][]string)
 	for _, du := range dUnits {
 		for _, f := range du.PlainFiles {
-			log.Printf("%s\n", f.ToString())
+			eSum := f.GetEncodedChecksum()
+			if _, ok := csMap[eSum]; ok {
+				csMap[eSum] = append(csMap[eSum], f.Name)
+			} else {
+				csMap[eSum] = []string{f.Name}
+			}
 		}
 	}
+	for _, v := range csMap {
+		if len(v) > 1 {
+			err := processDuplicates(v)
+			if err != nil {
+				log.Printf("Failed to process duplicates in %v -- (%v)\n", v, err)
+				os.Exit(-1)
+			}
+			log.Println("==================")
+		}
+	}
+}
+
+// //////////////////////////////////////////////////////////////////////////////////
+// Process duplicate files
+func processDuplicates(duplicates []string) error {
+	for _, f := range duplicates {
+		log.Printf("%s\n", f)
+	}
+	return nil
 }
